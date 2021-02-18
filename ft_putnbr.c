944511a45c8d904386194b5b6374f9af6e0edf8a
AhmedSakalov <56018472+AhmedSakalov@users.noreply.github.com>
2021-02-18 22:19:04 +0300

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,61 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fV1GD1rC.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raurelia <raurelia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 22:12:57 by raurelia          #+#    #+#             */
+/*   Updated: 2021/02/18 22:12:57 by raurelia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
- 
-void    f_zero(char nb)
+
+void	f_zero(char nb)
 {
-        if (nb == 0)
-        {
-                write(1, "0", 1);
-        }
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+	}
 }
- 
-void    f_flag_end(char f, char *c)
+
+void	f_flag_end(char f, char *c)
 {
-        if (f)
-        {
-                *c = '-';
-        }
+	if (f)
+	{
+		*c = '-';
+	}
 }
- 
-void    f_flag_begin(char *f, char *r, int *nb)
+
+void	f_flag_begin(char *f, char *r, int *nb)
 {
-        if (*nb < 0)
-        {
-                *f = 1;
-                *nb = -(*nb);
-                *r = 0;
-        }
+	if (*nb < 0)
+	{
+		*f = 1;
+		*nb = -(*nb);
+		*r = 0;
+	}
 }
- 
-void    f_range(char *b, char *r)
+
+void	f_range(char *b, char *r)
 {
-        while (*b != 0)
-        {
-                *b = *b / 10;
-                *r = *r + 1;
-        }
+	while (*b != 0)
+	{
+		*b = *b / 10;
+		*r = *r + 1;
+	}
 }
- 
-void    ft_putnbr(int nb)
+	
+void	ft_putnbr(int nb)
 {
-        char c[12];
-        char f;
-        char r;
-        char b;
-        char i;
- 
-        f = 0;
-        r = -1;
-        b = nb;
-        f_zero(nb);
-        f_flag_begin(&f, &r, &nb);
-        f_range(&b, &r);
-        i = 0;
-        while (i <= r)
-        {
-                c[r - i] = (nb % 10) + '0';
-                nb = nb / 10;
-                i++;
-        }
-        f_flag_end(f, &c[0]);
-        write(1, c, r + 1);
+	char c[12];
+	char f;
+	char r;
+	char b;
+	char i;
+
+	f = 0;
+	r = -1;
+	b = nb;
+	f_zero(nb);
+	f_flag_begin(&f, &r, &nb);
+	f_range(&b, &r);
+	i = 0;
+	while (i <= r)
+	{
+		c[r - i] = (nb % 10) + '0';
+		nb = nb / 10;
+		i++;
+	}
+	f_flag_end(f, &c[0]);
+	write(1, c, r + 1);
 }
