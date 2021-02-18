4fdbaaed2fd9d9120f2183c83bb42d98db651842
AhmedSakalov <56018472+AhmedSakalov@users.noreply.github.com>
2021-02-18 22:02:59 +0300

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,35 +1,61 @@
 #include <unistd.h>
-#include <stdio.h>
-
-void ft_putnbr(int a){
-    char c[12];
-    int f = 0;
-    //разрядность число
-    int r = -1;
-    int b = a;
-    
-    //флаг -
-    if(a < 0){
-        f++;
-        a = -a;
-        r = 0;
-    }
-    do{
-        b = b/10;
-        r++;
-    }while(b != 0);
-    
-    for(int i = 0; i <= r; i++){
-        c[r - i] = (a % 10) + '0';
-        a = a/10;
-    }
-    if(f){
-        c[0] = '-';
-    }
-    write(1,c,r+1);
+ 
+void    f_zero(char nb)
+{
+        if (nb == 0)
+        {
+                write(1, "0", 1);
+        }
 }
-
-int main(){
-    ft_putnbr(-0);
-    return 0;
+ 
+void    f_flag_end(char f, char *c)
+{
+        if (f)
+        {
+                *c = '-';
+        }
+}
+ 
+void    f_flag_begin(char *f, char *r, int *nb)
+{
+        if (*nb < 0)
+        {
+                *f = 1;
+                *nb = -(*nb);
+                *r = 0;
+        }
+}
+ 
+void    f_range(char *b, char *r)
+{
+        while (*b != 0)
+        {
+                *b = *b / 10;
+                *r = *r + 1;
+        }
+}
+ 
+void    ft_putnbr(int nb)
+{
+        char c[12];
+        char f;
+        char r;
+        char b;
+        char i;
+ 
+        f = 0;
+        r = -1;
+        b = nb;
+        f_zero(nb);
+        f_flag_begin(&f, &r, &nb);
+        f_range(&b, &r);
+        i = 0;
+        while (i <= r)
+        {
+                c[r - i] = (nb % 10) + '0';
+                nb = nb / 10;
+                i++;
+        }
+        f_flag_end(f, &c[0]);
+        write(1, c, r + 1);
 }
